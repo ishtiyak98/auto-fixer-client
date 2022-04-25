1df7716210c3f4496b8c39bbc7cf158435773aa1
ishtiyak98 <rifatishtiyak@gmail.com>
2022-04-25 18:12:21 +0600

    delete fuction added

--- src/Pages/ManageService/ManageCard/ManageCard.js
+++ src/Pages/ManageService/ManageCard/ManageCard.js
@@ -1,10 +1,35 @@
 import React from "react";
-import { Button, Card } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const ManageCard = ({ service }) => {
   const navigate = useNavigate();
   const { img, name, description, price, _id } = service;
+
+  const handleDelete = ()=> {
+    const proceed = window.confirm("Are your sure?");
+
+    if (proceed) {
+      console.log("deleting... ", _id);
+      
+      fetch(`http://localhost:5000/deleteService/${_id}`, {
+        method: "DELETE"
+      })
+      .then(res => res.json())
+      .then(data => {
+        if(data.acknowledged){
+          Swal.fire({
+              title: "Deleted",
+              text: "Successfully Deleted",
+              icon: "success",
+          });
+          navigate("/home");
+       }
+      })
+
+    }
+  }
 
   return (
     <div className="col-md-6 col-lg-4">
@@ -17,7 +42,7 @@ const ManageCard = ({ service }) => {
             <div className="d-flex justify-content-between align-items-center">
               <div>
                 <button className="btn btn-primary me-2" onClick={()=>{navigate(`/updateService/${_id}`)}}>Edit</button>
-                <button className="btn btn-danger me-2">Delete</button>
+                <button className="btn btn-danger me-2" onClick={handleDelete}>Delete</button>
               </div>
               <p className="mb-0 fw-bold fs-5 text-dark">${price}</p>
             </div>
